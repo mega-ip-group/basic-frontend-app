3d61ae0b897fe76fcf3798ef77189606d768c4aa
Ehab Melhem <Melhemehab@gmail.com>
2023-05-13 22:14:39 +0300

    setup react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import {useEffect, useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState("");
+    const dadJokes = () => {
+        fetch('/test-api/test-method')
+            .then(message => {
+                // console.log(message.json())
+                return message.json();
+            }).then(data=>{
+                console.log(data)
+        });
+    };
+
+    useEffect(() => {
+        console.log("stam")
+        dadJokes();
+    })
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    {message}
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
